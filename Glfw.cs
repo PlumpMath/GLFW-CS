db1992606ab73613a3054a39f4c4dd27e4b30e3b
ChevyRay <happytrash@gmail.com>
2015-08-05 15:07:16 -0700

    Added more functions, started on input and keyboard.

--- Glfw.cs
+++ Glfw.cs
@@ -19,6 +19,9 @@ namespace Glfw3
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void WindowFunc([MarshalAs(UnmanagedType.Struct)] Window window);
 
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    public delegate void WindowBoolFunc([MarshalAs(UnmanagedType.Struct)] Window window, [MarshalAs(UnmanagedType.Bool)] bool value);
+
     public enum ErrorCode
     {
         NotInitialized      = 0x00010001,
@@ -107,6 +110,156 @@ namespace Glfw3
         Visible     = 0x00020004,
         Decorated   = 0x00020005,
         Floating    = 0x00020007
+    }
+
+    public enum CursorMode
+    {
+        Normal      = 0x00034001,
+        Hidden      = 0x00034002,
+        Disabled    = 0x00034003
+    }
+
+    public enum KeyState
+    {
+        Release = 0,
+        Press   = 1,
+        Repeat  = 2
+    }
+
+    public enum KeyCode
+    {
+        Unknown         = -1,
+
+        Space           = 32,
+
+        Apostrophe      = 39,
+
+        Comma           = 44,
+        Minus           = 45,
+        Period          = 46,
+        Slash           = 47,
+        Alpha0          = 48,
+        Alpha1          = 49,
+        Alpha2          = 50,
+        Alpha3          = 51,
+        Alpha4          = 52,
+        Alpha5          = 53,
+        Alpha6          = 54,
+        Alpha7          = 55,
+        Alpha8          = 56,
+        Alpha9          = 57,
+        Semicolon       = 59,
+        Equal           = 61,
+        A               = 65,
+        B               = 66,
+        C               = 67,
+        D               = 68,
+        E               = 69,
+        F               = 70,
+        G               = 71,
+        H               = 72,
+        I               = 73,
+        J               = 74,
+        K               = 75,
+        L               = 76,
+        M               = 77,
+        N               = 78,
+        O               = 79,
+        P               = 80,
+        Q               = 81,
+        R               = 82,
+        S               = 83,
+        T               = 84,
+        U               = 85,
+        V               = 86,
+        W               = 87,
+        X               = 88,
+        Y               = 89,
+        Z               = 90,
+        LeftBracket     = 91,
+        Backslash       = 92,
+        RightBracket    = 93,
+
+        GraveAccent     = 96,
+
+        World1          = 161,
+        World2          = 162,
+
+        Escape          = 256,
+        Enter           = 257,
+        Tab             = 258,
+        Backspace       = 259,
+        Insert          = 260,
+        Delete          = 261,
+        Right           = 262,
+        Left            = 263,
+        Down            = 264,
+        Up              = 265,
+        PageUp          = 266,
+        PageDown        = 267,
+        Home            = 268,
+        End             = 269,
+
+        CapsLock        = 280,
+        ScrollLock      = 281,
+        NumLock         = 282,
+        PrintScreen     = 283,
+        Pause           = 284,
+
+        F1              = 290,
+        F2              = 291,
+        F3              = 292,
+        F4              = 293,
+        F5              = 294,
+        F6              = 295,
+        F7              = 296,
+        F8              = 297,
+        F9              = 298,
+        F10             = 299,
+        F11             = 300,
+        F12             = 301,
+        F13             = 302,
+        F14             = 303,
+        F15             = 304,
+        F16             = 305,
+        F17             = 306,
+        F18             = 307,
+        F19             = 308,
+        F20             = 309,
+        F21             = 310,
+        F22             = 311,
+        F23             = 312,
+        F24             = 313,
+        F25             = 314,
+
+        Keypad0         = 320,
+        Keypad1         = 321,
+        Keypad2         = 322,
+        Keypad3         = 323,
+        Keypad4         = 324,
+        Keypad5         = 325,
+        Keypad6         = 326,
+        Keypad7         = 327,
+        Keypad8         = 328,
+        Keypad9         = 329,
+        KeypadDecimal   = 330,
+        KeypadDivide    = 331,
+        KeypadMultiply  = 332,
+        KeypadSubtract  = 333,
+        KeypadAdd       = 334,
+        KeypadEnter     = 335,
+        KeypadEqual     = 336,
+
+        LeftShift       = 340,
+        LeftControl     = 341,
+        LeftAlt         = 342,
+        LeftSuper       = 343,
+        RightShift      = 344,
+        RightControl    = 345,
+        RightAlt        = 346,
+        RightSuper      = 347,
+        Menu            = 348,
+        Last            = Menu
     }
 
     [StructLayout(LayoutKind.Sequential)]
@@ -425,7 +578,7 @@ namespace Glfw3
         static extern int glfwWindowShouldClose(IntPtr window);
         public static bool WindowShouldClose(Window window)
         {
-            return glfwWindowShouldClose(window.Ptr) == 1;
+            return glfwWindowShouldClose(window.Ptr) != 0;
         }
 
         [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
@@ -539,7 +692,7 @@ namespace Glfw3
         static extern int glfwGetWindowAttrib(IntPtr window, int attrib);
         public static bool GetWindowAttrib(Window window, WindowAttrib attrib)
         {
-            return glfwGetWindowAttrib(window.Ptr, (int)attrib) == 1;
+            return glfwGetWindowAttrib(window.Ptr, (int)attrib) != 0;
         }
 
         [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
@@ -580,15 +733,89 @@ namespace Glfw3
         }
 
         [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
-        static extern IntPtr glfwSetWindowSizeCallback(IntPtr window, IntPtr callback);
-        public static void SetWindowSizeCallback(Window window, WindowSizeFunc callback)
+        static extern IntPtr glfwSetWindowCloseCallback(IntPtr window, IntPtr callback);
+        public static void SetWindowCloseCallback(Window window, WindowFunc callback)
         {
             var ptr = Marshal.GetFunctionPointerForDelegate(callback);
-            glfwSetWindowSizeCallback(window.Ptr, ptr);
+            glfwSetWindowCloseCallback(window.Ptr, ptr);
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr glfwSetWindowRefreshCallback(IntPtr window, IntPtr callback);
+        public static void SetWindowRefreshCallback(Window window, WindowFunc callback)
+        {
+            var ptr = Marshal.GetFunctionPointerForDelegate(callback);
+            glfwSetWindowRefreshCallback(window.Ptr, ptr);
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr glfwSetWindowFocusCallback(IntPtr window, IntPtr callback);
+        public static void SetWindowFocusCallback(Window window, WindowBoolFunc callback)
+        {
+            var ptr = Marshal.GetFunctionPointerForDelegate(callback);
+            glfwSetWindowFocusCallback(window.Ptr, ptr);
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr glfwSetWindowIconifyCallback(IntPtr window, IntPtr callback);
+        public static void SetWindowIconifyCallback(Window window, WindowBoolFunc callback)
+        {
+            var ptr = Marshal.GetFunctionPointerForDelegate(callback);
+            glfwSetWindowIconifyCallback(window.Ptr, ptr);
+        }
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        static extern IntPtr glfwSetFramebufferSizeCallback(IntPtr window, IntPtr callback);
+        public static void SetFramebufferSizeCallback(Window window, WindowSizeFunc callback)
+        {
+            var ptr = Marshal.GetFunctionPointerForDelegate(callback);
+            glfwSetFramebufferSizeCallback(window.Ptr, ptr);
         }
 
         [DllImport(dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "glfwPollEvents")]
         public static extern void PollEvents();
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "glfwWaitEvents")]
+        public static extern void WaitEvents();
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl, EntryPoint = "glfwPostEmptyEvent")]
+        public static extern void PostEmptyEvent();
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        static extern int glfwGetInputMode(IntPtr window, int mode);
+
+        [DllImport(dll, CallingConvention = CallingConvention.Cdecl)]
+        static extern void glfwSetInputMode(IntPtr window, int mode, int value);
+
+        public static CursorMode GetCursorMode(Window window)
+        {
+            return (CursorMode)glfwGetInputMode(window.Ptr, 0x00033001);
+        }
+
+        public static void SetCursorMode(Window window, CursorMode mode)
+        {
+            glfwSetInputMode(window.Ptr, 0x00033001, (int)mode);
+        }
+
+        public static bool GetStickyKeys(Window window)
+        {
+            return glfwGetInputMode(window.Ptr, 0x00033002) != 0;
+        }
+
+        public static void SetStickyKeys(Window window, bool enabled)
+        {
+            glfwSetInputMode(window.Ptr, 0x00033002, enabled ? 1 : 0);
+        }
+
+        public static bool GetStickyMouseButtons(Window window)
+        {
+            return glfwGetInputMode(window.Ptr, 0x00033003) != 0;
+        }
+
+        public static void SetStickyMouseButtons(Window window, bool enabled)
+        {
+            glfwSetInputMode(window.Ptr, 0x00033003, enabled ? 1 : 0);
+        }
     }
 }
 
